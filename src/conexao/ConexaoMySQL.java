a2ffc93b1977daa2a66fa3cf99ad75261cd17723
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-10-01 17:26:19 -0300

    Feat: criando verificação se esta com o banco ativo

--- src/conexao/ConexaoMySQL.java
+++ src/conexao/ConexaoMySQL.java
@@ -1,16 +1,22 @@
- 
 package conexao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class ConexaoMySQL {
-      private static final String URL = "jdbc:mysql://localhost:3306/apontamento1";
-        private static final String USUARIO = "root";
-        private static final String SENHA = "9823";
 
-        public static Connection obterConexao() throws SQLException {
+    private static final String URL = "jdbc:mysql://localhost:3306/apontamento1";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "9823";
+
+    public static Connection obterConexao() throws SQLException {
+        try {
             return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ERRO: " + e);
         }
+        return null;
+    }
 }
